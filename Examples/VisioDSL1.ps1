1e606afa7b691926a8146ee684210ee8c0650bb9
MikeShepard <mshepard70@gmail.com>
2016-03-25 18:33:15 -0500

    Initial DSL Example (doesn't work yet)

---
+++ Examples/VisioDSL1.ps1
@@ -0,0 +1,8 @@
+﻿Diagram C:\temp\TestVisio.vsdx -from c:\temp\Template.vstx
+Stencil Containers -BuiltIn
+Stencil Servers -From C:\stencil.vssx
+Shape WebServer –From Servers –Name ‘Web Server’
+Shape Location –From Containers –Name Domain
+Domain MyDomain {
+	WebServer MyServer
+}
