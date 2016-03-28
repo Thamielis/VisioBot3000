1e5d6e7d0c3e2950d934efdaac4eb4abdf5c3ee5
MikeShepard <mshepard70@gmail.com>
2016-03-28 15:38:06 -0500

    Update VisioDSL1.ps1
    Updated to better illustrate containers.

--- Examples/VisioDSL1.ps1
+++ Examples/VisioDSL1.ps1
@@ -1,8 +1,14 @@
-﻿Diagram C:\temp\TestVisio.vsdx -from c:\temp\Template.vstx
+Diagram C:\temp\TestVisio.vsdx -from c:\temp\Template.vstx
 Stencil Containers -BuiltIn
 Stencil Servers -From C:\stencil.vssx
 Shape WebServer –From Servers –Name ‘Web Server’
-Shape Location –From Containers –Name Domain
+Shape Location –From Containers –Name 'Physical Location'
+Shape Domain -From Containers -Name 'AD Domain'
 Domain MyDomain {
-	WebServer MyServer
+	Location MyCity {
+		WebServer PrimaryServer
+	}
+	Location DRSite {
+		WebServer BackupServer
+	}
 }
