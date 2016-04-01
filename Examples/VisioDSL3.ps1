0902441c3f00a9efd2b6796eed12e650313f93e6
MikeShepard <mike_sh@MIKE-SHE-LAP3.jhacorp.com>
2016-04-01 09:01:06 -0500

    Added comments to DSL3

--- Examples/VisioDSL3.ps1
+++ Examples/VisioDSL3.ps1
@@ -1,17 +1,20 @@
-﻿stop-process -Name VISIO -ea SilentlyContinue
+﻿#clean-up because I'm re-running this over and over
+stop-process -Name VISIO -ea SilentlyContinue
 remove-item c:\temp\testvisio3.vsdx -ea SilentlyContinue
 import-module VisioBot3000 -Force
 
 Diagram C:\temp\TestVisio3.vsdx 
+
+# Define shapes, containers, and connectors for the diagram
 Stencil Containers -From C:\temp\MyContainers.vssx 
 Stencil Servers -From C:\temp\SERVER_U.vssx
 Shape WebServer -From Servers -MasterName 'Web Server'
 Container Location -From Containers -MasterName 'Location'
 Container Domain -From Containers -MasterName 'Domain'
 Container Logical -From Containers -MasterName 'Logical'
-Connector SQL -Color Red -arrow
+Connector SQL -Color Red -arrow 
 
-
+#this is the diagram
 Logical MyFarm {
     Location MyCity {
         Domain MyDomain {
@@ -23,5 +26,5 @@ Logical MyFarm {
 		    WebServer BackupServer 5 8
 	    }
     }
-    SQL -From PrimaryServer -To BackupServer
 }
+SQL -From PrimaryServer -To BackupServer
