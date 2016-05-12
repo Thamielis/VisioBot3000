3319a579b54f6cfc69bbb56d5ba26b949c29475f
MikeShepard <mshepard70@gmail.com>
2016-05-11 22:35:02 -0500

    Added Convert-VisioObjectToPSObject function (in VisioExport.ps1).  Fixed #53

---
+++ VisioExport.ps1
@@ -0,0 +1,25 @@
+﻿function Convert-VisioObjectToPSObject{
+[CmdletBinding()]
+Param([Parameter(ValueFromPipeline=$true)]$object) 
+    
+    $objectHash=@{Name=$object.Name;
+                      Text=$object.Text}
+    if(($object | get-member Master) -and $object.Master){
+       $objectHash.Add('Type',$object.Master.Name)
+    }
+    if($object | get-member PageSheet){
+        #get the shapes that aren't in containers
+        $containedObjects= $object.Shapes | Where-Object {$_.MemberOfContainers.Count -eq 0} |foreach {Export-VisioObject $_}
+    } elseif($object | get-member ContainerProperties){
+        #get the top-level objects contained
+        if($object.ContainerProperties){
+            $containedObjectIDs=$object.ContainerProperties.GetMemberShapes(16+2) 
+            $containedObjects=$ContainedObjectIDs|foreach {Export-VisioObject ($object.ContainingPage.Shapes | WHERE id -EQ $_)}
+        }
+    }
+    if($containedObjects){
+        $objectHash.Add('Contents',$containedObjects)
+    }
+    [PSCustomObject]$objectHash
+
+}
