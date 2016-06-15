9825821b06e6f7dc2be25664bba24caa499c1e4b
MikeShepard <mshepard70@gmail.com>
2016-06-14 19:16:52 -0500

    Added support for VisioSettings (Import-VisioSettings) and included an example script and settings file.

---
+++ Examples/VisioSettingsExample.ps1
@@ -0,0 +1,21 @@
+﻿Import-Module VisioBot3000 -Force
+ 
+
+#start Visio and create a new document
+New-VisioApplication
+New-VisioDocument C:\temp\TestVisioPrimitives.vsdx 
+
+$doc=Get-VisioDocument
+Set-VisioDiagramServices -Document $doc -Value $vis.ServiceAll
+
+#adjust path to match the location you put the setting file.
+Import-VisioSettings -path  C:\Users\mike\Documents\WindowsPowerShell\modules\VisioBot3000\Examples\DiagramSettings.psd1 
+
+#draw a container with two items in it
+Domain   MyDomain {
+     WebServer PrimaryServer 
+     DBServer SQL01  
+}
+
+#add a connector
+SQL -from PrimaryServer -To SQL01
